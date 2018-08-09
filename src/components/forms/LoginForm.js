6d510ec1c77272c856de3448462f42597d0798a2
wasifmiraz <wasifmiraz@mail.com>
2018-08-09 20:04:39 +0600

    form validate

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,17 +1,71 @@
 import React, { Component } from 'react'
 import {Form, Button} from 'semantic-ui-react'
+import validator from 'validator'
 
 export default class LoginForm extends Component {
     state= {
-        data:{},
+        data:{
+        emai:"",
+        password:"",
+        },
         loading:false,
         erorr:{}
       
     }
+
+    onChange = (e) => {
+        this.setState({
+            data:{...this.state.data,[e.target.name]:e.target.value}
+        })
+    }
+
+
+onSubmit = () => {
+     const error = this.validate(this.state.data);
+     this.setState ( {
+         error
+     });
+};
+
+validate = (data) => {
+    const error = {};
+if(!validator.isEmail(data.emai)) error.email = "can't be invalid"
+ if(!data.password) error.password ="can't be blink";
+    return error;
+}
+
+
+
   render() {
+      const {data} = this.state;
     return (
       <div>
-        <Form>
+        <Form onSubmit={this.onSubmit}>
+        <Form.Field>
+        <label htmlfor="email"> Email</label>
+        <input 
+        type="email"
+        id="email"
+        name="email"
+        placeholder="example@gmail.com"
+        value={data.email}
+        onChange={this.onChange}
+        >
+         </input>
+        </Form.Field>
+
+        <Form.Field>
+        <label htmlfor="Password"> Password</label>
+        <input 
+        type="password"
+        id="password"
+        name="password"
+        placeholder="password"
+        value={data.password}
+        onChange={this.onChange}
+        >
+         </input>
+        </Form.Field>
           <Button className="primary">Login</Button>
         </Form>
         
