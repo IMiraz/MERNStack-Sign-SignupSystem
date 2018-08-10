01318dba3ad5331eedb195377e1277e38b5f67a3
wasifmiraz <wasifmiraz@mail.com>
2018-08-10 20:58:38 +0600

    redux dev tools setup

--- src/index.js
+++ src/index.js
@@ -5,15 +5,21 @@ import{ BrowserRouter } from 'react-router-dom'
 import {createStore,applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
 import thunk from 'redux-thunk'
+
+import { composeWithDevTools } from 'redux-devtools-extension';
 import 'semantic-ui-css/semantic.min.css';
 import registerServiceWorker from './registerServiceWorker';
 import rootReducer from './components/Reducer/rootReducer'
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const store = createStore(rootReducer,
+    composeWithDevTools(applyMiddleware(thunk)
+))
 
 ReactDOM.render(
     <BrowserRouter>
+    <Provider store={store}>
     <App />
+    </Provider>
     </BrowserRouter>,
  document.getElementById('root'));
 registerServiceWorker();
