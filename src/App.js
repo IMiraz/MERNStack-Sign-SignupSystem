87f4a2f055199ce68e5b4521abf4810705971d3f
wasifmiraz <wasifmiraz@mail.com>
2018-08-15 15:19:47 +0600

    router authorized done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
+import propTypes from 'prop-types'
 import {Route} from 'react-router-dom'
 import Homepage from './components/pages/HomePage'
 import LoginPage from './components/pages/LoginPage'
 import DashboradPage from './components/pages/DashboardPage'
 import UserRoute from './components/Router/UserRoute'
+import GuestRoute from './components/Router/GuestRoute'
 
 
 
-const App = () => (
+const App = ({location}) => (
 <div className="ui container">
-<Route path="/" exact component={Homepage} />
-<Route path="/login"  component={LoginPage} />
-<UserRoute path="/dashborad"  component={DashboradPage} />
+<Route  location={location} path="/" exact component={Homepage} />
+<GuestRoute location={location} path="/login"  component={LoginPage} />
+<UserRoute location={location} path="/dashborad"  component={DashboradPage} />
 </div>
 );
+App.propTypes={
+location:propTypes.shape({
+
+pathname:propTypes.string.isRequired
+
+}).isRequired
+}
 
 export default App;
