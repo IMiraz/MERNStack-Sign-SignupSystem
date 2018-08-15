1e665f9fe780252a9f45c28352d191b46bf402c4
wasifmiraz <wasifmiraz@mail.com>
2018-08-15 11:51:35 +0600

    logout return

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {connect} from 'react-redux'
+import propTypes from 'prop-types'
+import {Button} from 'semantic-ui-react'
 
 
-const Homepage = () => (
+const Homepage = ({isAuthenticated}) => (
     <div>
  <h1>home page welcome</h1>
- <Link to="/login">Login</Link>
-    </div>
-    );
 
-export default Homepage;
+{isAuthenticated?  <Button content="Logout"/>:<Link to="/login">Login</Link>}</div>
+
+);
+
+Homepage.propTypes = {
+
+    isAuthenticated:propTypes.bool.isRequired
+
+}
+function mapStateToProps(state){
+return {
+    isAuthenticated:!! state.user.token
+
+};
+}
+
+export default connect(mapStateToProps) (Homepage);
